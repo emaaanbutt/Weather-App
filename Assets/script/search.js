af1d02ad3997b63daf241c5f0e139b0f6cc94ac6
Eman Butt <butt.eman391@gmail.com>
2025-05-08 21:10:19 +0500

    City API fixed.

--- Assets/script/search.js
+++ Assets/script/search.js
@@ -15,57 +15,58 @@ function searchCity()
 }
 
 
+$(function() {
+    $('#cityInput').on('input', function() {
+        var query = $(this).val().trim();
 
-// $(function() {
-//     $('#cityInput').on('input', function() {
-//         var query = $(this).val().trim();
+        if (query.length < 2) {
+            $('#suggestions').empty().hide(); 
+            return;
+        }
 
-//         if (query.length < 2) {
-//             $('#suggestions').empty().hide(); 
-//             return;
-//         }
+        let api_url = 'https://api.api-ninjas.com/v1/city?name=' + encodeURIComponent(query);
 
-//         let api_url = 'https://api.api-ninjas.com/v1/city?name=' + encodeURIComponent(query) + '&limit=5';
+        console.log("API URL:", api_url);
 
-//         $.ajax({
-//             url: api_url,
-//             method: 'GET',
-//             headers: {
-//                 'X-Api-Key': 'd/X07qv9XuiDWVpGkNL8g==ZbJlrqForjZq9w9F'
-//             },
-//             success: function(response) {
-//                 $('#suggestions').empty();
+        $.ajax({
+            url: api_url,
+            method: 'GET',
+            headers: {
+                'X-Api-Key': 'e2t23LDvCmIw6xJIYTrzmA==X1kKBQZSP29HObc8'
+            },
+            success: function(response) {
+                console.log("API Response:", response);
+                $('#suggestions').empty();
 
-//                 if (response.length === 0) {
-//                     $('#suggestions').hide();
-//                     return;
-//                 }
+                if (response.length === 0) {
+                    $('#suggestions').hide();
+                    return;
+                }
 
-//                 response.forEach(function(city) {
-//                     $('#suggestions').append('<li>' + city.name + '</li>');
-//                 });
+                response.forEach(function(city) {
+                    $('#suggestions').append('<li>' + city.name + '</li>');
+                });
 
-//                 $('#suggestions').show(); 
+                $('#suggestions').show(); 
 
-//                 $('#suggestions li').on('click', function() {
-//                     $('#cityInput').val($(this).text());
-//                     $('#suggestions').empty().hide();
-//                 });
-//             },
-//             error: function(error) {
-//                 console.error('Autocomplete error:', error);
-//                 $('#suggestions').hide();
-//             }
-//         });
-//     });
+                $('#suggestions li').on('click', function() {
+                    $('#cityInput').val($(this).text());
+                    $('#suggestions').empty().hide();
+                });
+            },
+            error: function(error) {
+                console.error('Autocomplete error:', error);
+                $('#suggestions').hide();
+            }
+        });
+    });
 
-//     $(document).on('click', function(e) {
-//         if (!$(e.target).closest('#cityInput, #suggestions').length) {
-//             $('#suggestions').empty().hide();
-//         }
-//     });
-// });
-
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('#cityInput, #suggestions').length) {
+            $('#suggestions').empty().hide();
+        }
+    });
+});
 
 
 
