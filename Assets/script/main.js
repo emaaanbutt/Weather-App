262d3828cc75d65c4c3767c5ff855d71e35b31af
Eman Butt <butt.eman391@gmail.com>
2025-05-08 21:39:31 +0500

    Added day.

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -1,3 +1,30 @@
 let city = localStorage.getItem("current-city");
 document.querySelector("#city h2").textContent = city;
 
+
+$(function(){
+
+    const appid = "ffe8ec1265ed9514e5cdf47c055d0529";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=" + unit + "&appid="+ appid+""; 
+
+
+    let dateTime = new Date();
+    let day = dateTime.toLocaleString('en-US', { weekday: 'long' });
+    $("#day").text(day);
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+
+
+
+        }
+    });
+
+});
+    
